34b52289e7ee730c94681892b8e08182ca10be81
Tran Nguyen Khang <nguyenkhang240596@gmail.com>
2016-09-09 20:09:01 +0700

    update

--- models/user.js
+++ models/user.js
@@ -1,32 +1,59 @@
 var mongoose = require('mongoose');
 var	Schema = mongoose.Schema;
+var crypto = require('crypto');
 
-var UserSchema = new Schema({
-	name : {
+var User = new Schema({
+	email : {
+		type : String
+		// ,
+		// unique : true
+	},
+	phone : String,
+	image : {
 		type : String
 	},
-	assessTo : [{
-		type : Schema.Types.ObjectId,
-		ref : 'User'
-	}],
-	assessFrom : {
-		type : Schema.Types.ObjectId,
-		ref : 'User'
+	birthday : {
+		type : Date
 	},
-	content : {
-		type : String
+	gender : {
+		type : Boolean,
+		default : false
 	},
-	creatorId : {
-		type : Schema.Types.ObjectId,
-		ref : ''
-	},
-	createdDate : {
-		type : Date,
-		default : Date.now
-	}
+	hashed_password : String,
+	salt : String
 },{
 	versionKey : false
 });
 
 
-module.exports = Assess;
+User.virtual('password').set(function(password) {
+  	this._password = password;
+  	this.salt = this.makeSalt();
+  	this.hashed_password = this.hashPassword(password);
+}).get(function() {
+  return this._password;
+});
+
+User.methods ={
+	authenticate: function(plainText) {
+    	return this.hashPassword(plainText) === this.hashed_password;
+	},
+	makeSalt: function() {
+    	return crypto.randomBytes(16).toString('base64');
+	},
+	hashPassword: function(password) {
+    	if (!password || !this.salt) return '';
+    	var salt = new Buffer(this.salt, 'base64');
+    	return crypto.pbkdf2Sync(password, salt, 071189, 64).toString('base64');
+	},
+
+	toJSON: function() {
+		var obj = this.toObject();
+		delete obj.hashed_password;
+		delete obj.salt;
+		return obj;
+	}
+}
+
+
+module.exports = User;
